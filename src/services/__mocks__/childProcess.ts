229ed826d892df74633bfe201e86e24c32ea03c3
BTaduran <btaduran30@protonmail.com>
2025-12-30 16:22:04 +0800

    update __mocks__ for childProcess.ts to use spy
    - add mock for getVideoDuration function

--- src/services/__mocks__/childProcess.ts
+++ src/services/__mocks__/childProcess.ts
@@ -1,6 +1,15 @@
+import {vi} from "vitest";
 import {fs} from "memfs";
 import {type ExecSyncOptions} from "node:child_process";
 
-export const mergeVideoSegments = (_segmentListFilename: string, outputFilename: string, _execSyncOptions: ExecSyncOptions) => {
-    fs.writeFileSync(outputFilename, "something")
-}
+export const mergeVideoSegments =
+    vi.fn(
+        (_segmentListFilename: string, outputFilename: string, _execSyncOptions: ExecSyncOptions) => {
+            fs.writeFileSync(outputFilename, "something")
+        }
+    )
+
+export const getVideoDuration = vi.fn(
+    (_baseOutputPath: string, _file: string) => {
+        throw new Error(`Mock return value for ${getVideoDuration.name} is not implemented.`)
+    })
